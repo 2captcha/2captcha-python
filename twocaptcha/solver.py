61d75a8185b6273d41858aa5dcdaac3532b18523
asenatorrr <61660057+asenatorrr@users.noreply.github.com>
2024-06-18 14:28:08 +0300

    Fix imginstructions key in TwoCaptcha/check_hint_img method (#86)

--- twocaptcha/solver.py
+++ twocaptcha/solver.py
@@ -1028,6 +1028,7 @@ class TwoCaptcha():
             return params, files
 
         if not '.' in hint and len(hint) > 50:
+            params.update({'imginstructions': hint})
             return params, files
 
         if not os.path.exists(hint):
